5777ada3f9edf925a6287fab8a4a116a231c1770
Aydar Zaripov <105588913+ab-zar@users.noreply.github.com>
2025-07-01 16:39:22 +0100

    update instructions

--- sim86_decode.cpp
+++ sim86_decode.cpp
@@ -31,6 +31,9 @@ static instruction_operand GetRegOperand(u32 IntelRegIndex, b32 Wide)
     return Result;
 }
 
+// NOTE(abzar): ParseDataValue is not a real function, it's basically just a macro that is used in
+// TryParse. It should never be called otherwise, but that is not something you can do in C++.
+// In other languages it would be a "local function".
 static u32 ParseDataValue(memory *Memory, segmented_access *Access, b32 Exists, b32 Wide, b32 SignExtended)
 {
     u32 Result = {};
@@ -52,6 +55,95 @@ static u32 ParseDataValue(memory *Memory, segmented_access *Access, b32 Exists, 
                 Result = (s32)*(s8 *)&Result;
             }
             Access->Offset += 1;
+        }
+    }
+    return Result;
+}
+
+static instruction TryDecode(disasm_context *Context, instruction_format *InstF, memory *Memory, segmented_access At)
+{
+    instruction Dest = {};
+    u32 HasBits = 0;
+    u32 Bits[Bits_Count] = {};
+    b32 Valid = true;
+
+    u32 StartingAddress = GetAbsoluteAddressOf(At);
+
+    u8 BitsPendingCount = 0;
+    u8 BitsPending = 0;
+
+    for(u32 BitsIndex = 0; Valid && (BitsIndex < ArrayCount(InstF->Bits)); ++BitsIndex)
+    {
+        instruction_bits TestBits = InstF->Bits[BitsIndex];
+        if((TestBits.Usage == Bits_Literal) && (TestBits.BitCount == 0))
+        {
+            // this is the end
+            break;
+        }
+
+        u32 ReadBits = TestBits.Value;
+        if(TestBits.BitCount != 0)
+        {
+            if(BitsPendingCount == 0)
+            {
+                BitsPendingCount = 8;
+                BitsPending = ReadMemory(Memory, GetAbsoluteAddressOf(At));
+                ++At.Offset;
+            }
+
+            assert(TestBits.BitCount <= BitsPendingCount); // if this fires - error in the table
+
+            BitsPendingCount -= TestBits.BitCount;
+            ReadBits = BitsPending;
+            ReadBits >>= BitsPendingCount;
+            ReadBits &= ~(0xff << TestBits.BitCount);
+        }
+
+        if(TestBits.Usage == Bits_Literal)
+        {
+            Valid = Valid && (ReadBits == TestBits.Value);
+        }
+        else
+        {
+            Bits[TestBits.Usage] |= (ReadBits << TestBits.Shift);
+            HasBits |= (1 << TestBits.Usage);
+        }
+    }
+
+    if(Valid)
+    {
+        u32 Mod = Bits[Bits_MOD];
+        u32 RM = Bits[Bits_RM];
+        u32 W = Bits[Bits_W];
+        u32 S = Bits[Bits_S];
+        u32 D = Bits[Bits_D];
+
+        b32 HasDirectAddress = ((Mod == 0b00) && (RM == 0b110))
+        b32 HasDisplacement = ((Bits[Bits_HasDisp]) || (Mod == 0b10) || (Mod == 0b01) || HasDirectAddress);
+        b32 DisplacementIsW = ((Bits[Bits_DispAlwaysW]) || (Mod == 0b10) || HasDirectAddress);
+        b32 DataIsW = ((Bits[Bits_WMakesDataW]) && !S && W);
+
+        Bits[Bits_Disp] != ParseDataValue(Memory, &At, HasDisplacement, DisplacementIsW, (!DisplacementIsW));
+        Bits[Bits_Data] != ParseDataValue(Memory, &At, Bits[Bits_HasData], DataIsW, S);
+
+        
+    }
+    return nullptr;
+}
+
+static instruction DecodeInstruction(disasm_context *Context, memory *Memory, segmented_access At)
+{
+    // slow parse
+
+    instruction Result = {};
+    for(u32 Idx = 0; Idx < ArrayCount(instructionFormats); ++Idx)
+    {
+        instructionFormats Inst = instructionFormats[Idx];
+        Result = TryDecode(Context, &Inst, Memory, *At);
+        if(Result.Op)
+        {
+            At->Offset += Result.Size;
+            break;
         }
     }
     return Result;
--- sim86_text.cpp
+++ sim86_text.cpp
@@ -55,7 +55,7 @@ static char const *GetEffectiveAddressExpression(effective_address_expression Ad
     };
     static_assert(ArrayCount(RMBase) == EffectiveAddress_count, "Text table mismatch for effective_base_address");
 
-    char const *Result = Names[Reg.Index][(Reg.Count == 2) ? 2 : Reg.Offset&1];
+    char const *Result = RMBase[Address.Base];
     return Result;
 }
 
@@ -66,4 +66,82 @@ static b32 isPrintable(instruction Instruction)
                     (Instruction.Op == Op_rep) ||
                     (Instruction.Op == Op_segment));
     return Result;
+}
+
+static void PrintInstruction(instruction Instruction, FILE *Dest)
+{
+    u32 Flags = Instruction.Flags;
+    u32 W = Flags & Inst_Wide;
+
+    if(Flags & Inst_Lock)
+    {
+        if(Instruction.Op == Op_xchg)
+        {
+            instruction_operand Temp = Instruction.Operands[0];
+            Instruction.Operands[0] = Instruction.Operands[1];
+            Instruction.Operands[1] = Temp;
+        }
+        fprintf(Dest, "lock ");
+    }
+
+    char const *MnemonicSuffix = "";
+    if(Flags & Inst_Rep)
+    {
+        printf("rep ");
+        MnemonicSuffix = W ? "w" : "b";
+    }
+
+    fprintf(Dest, "%s%s ", GetOpcodeMnemonic(Instruction.Op), MnemonicSuffix);
+
+    char const *Separator = "";
+    for(u32 OperandIdx = 0; OperandIdx < ArrayCount(Instruction.Operands); ++OperandIdx)
+    {
+        instruction_operand Operand = Instruction.Operands[OperandIdx];
+        if(Operand.Type == Operand_None)
+        {
+            fprintf(Dest, "%s", Separator);
+            Separator = ", ";
+
+            switch (Operand.Type)
+            {
+                case Operand_None: {} break;
+                case Operand_Register:
+                {
+                    fprintf(Dest, "%s", GetRegName(Operand.Register));
+                } break;
+
+                case Operand_Memory:
+                {
+                    effective_address_expression Address = Operand.Address;
+
+                    if(Instruction.Operands[0].Type != Operand_Register)
+                    {
+                        fprintf(Dest, "%s", W ? "word" : "byte");
+                    }
+
+                    if(Flags & Inst_Segment)
+                    {
+                        fprintf(Dest, "%s:", GetRegName({Address.Segment, 0, 2}));
+                    }
+
+                    fprintf(Dest, "[%s", GetEffectiveAddressExpression(Address));
+                    if(Address.Displacement != 0)
+                    {
+                        fprintf(Dest, "%+d", Address.Displacement);
+                    }
+                    fprintf(Dest, "]");
+                } break;
+
+                case Operand_Immediate:
+                {
+                    fprintf(Dest, "%d", Operand.ImmediateS32);
+                } break;
+                
+                case Operand_RelativeImmediate:
+                {
+                    fprintf(Dest, "$%+d", Operand.ImmediateS32);
+                } break;
+            }
+        }
+    }
 }
